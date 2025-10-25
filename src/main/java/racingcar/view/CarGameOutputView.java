2e8bfe7aca53f851d1135c86958399c13131c53c
Runea <otter2023@naver.com>
2025-10-25 22:45:26 +0900

    feat(view): 사용자 입출력 클래스(InputView, OutputView) 구현

---
+++ src/main/java/racingcar/view/CarGameOutputView.java
@@ -0,0 +1,34 @@
+package racingcar.view;
+
+import racingcar.domain.Car;
+import racingcar.domain.Round;
+
+import java.util.List;
+
+public class CarGameOutputView {
+
+    public void interimResult(Round thisRound) {
+        System.out.println("실행 결과");
+        List<Car> cars = thisRound.getCars();
+
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getStep()));
+        }
+        System.out.println();
+    }
+
+    public void finalResult(Round result) {
+        System.out.print("최종 우승자 : ");
+        System.out.println(result);
+    }
+
+    public void result(List<Round> rounds) {
+        for (Round round : rounds) {
+            interimResult(round);
+        }
+
+        Round finalRound = rounds.getLast(); // Java 21+ 에만 존재
+        finalResult(finalRound);
+    }
+
+}
